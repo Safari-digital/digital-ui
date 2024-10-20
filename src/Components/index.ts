289b5b61595bcc2ea49c66dc1351a0807b34949c
BenoitSafari <gregory.michalak.192@gmail.com>
2024-10-20 19:11:38 +0100

    Fixed missing Form import

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -3,6 +3,7 @@ export * from './types';
 export * from './Avatar';
 export * from './Box';
 export * from './Button';
+export * from './Form';
 export * from './Header';
 export * from './Icon';
 export * from './InputSwitch';
@@ -11,3 +12,4 @@ export * from './Loader';
 export * from './PopOver';
 export * from './Text';
 export * from './User';
+
